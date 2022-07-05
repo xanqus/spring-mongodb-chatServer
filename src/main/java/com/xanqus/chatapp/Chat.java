d8c9cbe3dbb0ee3dc5ab4260e213267f24d1a2d6
xanqus <sbk98031816@gmail.com>
2022-07-06 03:13:55 +0900

    @Tailable

---
+++ src/main/java/com/xanqus/chatapp/Chat.java
@@ -0,0 +1,18 @@
+package com.xanqus.chatapp;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Data
+@Document(collection = "chat")
+public class Chat {
+    @Id
+    private String id;
+    private String msg;
+    private String sender;// 보내는 사람
+    private String receiver;// 받는 사람
+    private LocalDateTime createdAt;
+}
